13206bd1a7aacb3a5e585779e40fb174f2eaca7b
Konrad Christian <ckonrad22@gmail.com>
2022-07-24 23:05:29 -0300

    Entrega tercer trabajo Arrays

--- JS/script1.js
+++ JS/script1.js
@@ -6,15 +6,33 @@ if (nombre != "") {
     alert("Bienvenido internauta!")
 }
 
-let nombreproductoA = "adulto"
-let precioproductoA = "20000"
-let stockproductoA = "7"
+function Producto(nombre, stock, precio){
+    this.nombre = nombre;
+    this.precio = precio;
+    this.stock = stock;
+    
+}
 
-let nombreproductoB = "chicos"
-let precioproductoB = "5000"
-let stockproductoB = "5"
+const productoA = new Producto("adulto", 20000, 7);
+const productoB = new Producto("chicos", 5000, 5);
+const productoC = new Producto("triciclo", 1000, 10);
+const productoD = new Producto("monopatin", 1500, 12);
+const productoE = new Producto("rollers", 2000, 7);
 
-let cantidadcompras = prompt("Por favor ingrese la cantidad de compras que va a realizar (1 o 2)")
+let cantidadcompras = prompt("Por favor ingrese la cantidad de compras que va a realizar:")
+
+/*Se adiciona función Arrays*/
+let listaProductos = [productoA, productoB, productoC, productoD, productoE] 
+
+TotalDeProductos = []
+
+function listarproductos(){
+    for(const producto of listaProductos)
+        TotalDeProductos.push(producto.nombre)
+}
+
+listarproductos()
+
 let precioTotal = 0;
 
 function calculoPrecio(precio, cantidad){
@@ -22,7 +40,7 @@ function calculoPrecio(precio, cantidad){
     
 }
 
-function calculoStock(cantidad, stock, precio){
+function calculoStock(cantidad, precio, stock,){
     if(stock >= cantidad){
         calculoPrecio(precio, cantidad)
         alert("El precio de su compraes: $" + (precio * cantidad))
@@ -34,19 +52,37 @@ function calculoStock(cantidad, stock, precio){
 
 for(let i = 0; i < cantidadcompras; i++){
 
-    let compra1 = prompt("Ingrese el tipo de bicicletas que desea comprar: \n- adulto\n- chicos")
+    let compra1 = prompt("Ingrese el nombre del producto que desea comprar: \n-" + TotalDeProductos.join("\n-")).toLocaleLowerCase() 
     let cantidad1 = prompt("Ingrese la cantidad del producto que desea comrpar")
 
     if(compra1 == "adulto"){
-        calculoStock(cantidad1, stockproductoA, precioproductoA)
+        calculoStock(cantidad1, productoA.stock, productoA.precio)
+        ColorDeProductos()
     }
 
     else if(compra1 == "chicos"){
-        calculoStock(cantidad1, stockproductoB, precioproductoB)
+        calculoStock(cantidad1, productoB.stock, productoB.precio)
+        ColorDeProductos()
+    }
+
+    else if(compra1 == "triciclo"){
+        calculoStock(cantidad1, productoC.stock, productoC.precio)
+        ColorDeProductos()
+    }
+
+    else if(compra1 == "monopatin"){
+        calculoStock(cantidad1, productoD.stock, productoD.precio)
+        ColorDeProductos()
+    }
+
+    else if(compra1 == "rollers"){
+        calculoStock(cantidad1, productoE.stock, productoE.precio)
+        ColorDeProductos()
     }
 
     else {
         alert("No disponemos de ese producto")
+        compra1 = prompt("Ingrese el nombre del producto que desea comprar: \n-" + TotalDeProductos.join("\n-")).toLocaleLowerCase()
     }
 }
 
@@ -55,28 +91,44 @@ switch(true){
     case precioTotal >= 10000 && precioTotal <= 15000:
         precioTotal = precioTotal * 0.95
         alert("Recibiste un descuento del 5% por tu compra")
+        alert("El precio total de su compra con descuento es: $" + precioTotal)
+        console.log()
+        alert("Compra realizada con exito!")
         break;
     case precioTotal >= 16000 && precioTotal <= 25000:
         precioTotal = precioTotal * 0.90
         alert("Recibiste un descuento del 10% por tu compra")
+        alert("El precio total de su compra con descuento es: $" + precioTotal)
+        console.log()
+        alert("Compra realizada con exito!")
         break;
     case precioTotal >= 26000 && precioTotal <= 35000:
         precioTotal = precioTotal * 0.80
         alert("Recibiste un descuento del 20% por tu compra")
+        alert("El precio total de su compra con descuento es: $" + precioTotal)
+        console.log()
+        alert("Compra realizada con exito!")
         break;
     case precioTotal > 45000:
-        alert("Recibiste un descuento del 40% por tu compra")
+        
         precioTotal = precioTotal * 0.60
+        alert("Recibiste un descuento del 40% por tu compra")
+        alert("El precio total de su compra con descuento es: $" + precioTotal)
+        console.log()
+        alert("Compra realizada con exito!")
         break;
     default:
         console.log(precioTotal)
         alert("Para recibir de nuestros descuentos su compra debe superar los $10000");
+        console.log()
+        alert("Compra realizada con exito!")
         break;
 }
     
-let mensajeColorError = "Error: Debe ingresar un color disponible: verde/rojo/negro/azul";
-
-let color = prompt("Ingrese color que desea verde/rojo/negro/azul")
+/*Se agrega funcion para que la seleccion de color sea por cada uno de los productos a comprar*/
+function ColorDeProductos(){
+    for(let e = 1; e <= 1; e++)
+    color = prompt("Ingrese color que desea verde/rojo/negro/azul")
 
     if((color == "verde") || (color == "rojo") || (color == "negro") || (color == "azul")) {
         alert("Color en stock")
@@ -85,5 +137,10 @@ let color = prompt("Ingrese color que desea verde/rojo/negro/azul")
             alert(mensajeColorError)
         };
 
-    console.log()
-        alert("Compra realizada con exito!")
+
+let mensajeColorError = "Error: Debe ingresar un color disponible: verde/rojo/negro/azul";
+}
+
+
+
+   
